95555929bb3d41560942daeb37adf300319a444a
ANAND P <143876084+ANAND0Z@users.noreply.github.com>
2025-04-16 21:12:22 +0530

    Update FileManager.java

--- FileManager.java
+++ FileManager.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class FileManager {
     private static final String USER_FILE = "users.txt";
+    private static final String ADMIN_FILE = "admins.txt";
 
     public static void saveUsers(List<User> users){
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(USER_FILE))) {
@@ -20,6 +21,23 @@ public class FileManager {
             System.out.println("Error loading users data.");
         }
         return new ArrayList<>();
+    }
+
+    public static List<Admin> loadadmins(){
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(ADMIN_FILE))) {
+            return (List<Admin>) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error loading admin data.");
+        }
+        return new ArrayList<>();
+    }
+
+    public static void saveadmins(List<Admin> users){
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(ADMIN_FILE))) {
+            out.writeObject(users);
+        } catch (IOException e) {
+            System.out.println("Error saving admin data.");
+        }
     }
 }
 
